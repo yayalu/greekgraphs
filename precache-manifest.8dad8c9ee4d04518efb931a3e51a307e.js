070e6a2ff241b8353d9d7a4a25646e93e940413b
Yaya Lu <yaya.lu.01@gmail.com>
2019-10-22 14:49:52 +1100

    Updates

---
+++ precache-manifest.8dad8c9ee4d04518efb931a3e51a307e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b15006cb1a9b878a100867f0ad991881",
+    "url": "/greekgraphs/index.html"
+  },
+  {
+    "revision": "9edfafa7a5fc3dd95924",
+    "url": "/greekgraphs/static/css/main.b744c8f8.chunk.css"
+  },
+  {
+    "revision": "efa3d7b277cdb7d1505c",
+    "url": "/greekgraphs/static/js/2.0d097137.chunk.js"
+  },
+  {
+    "revision": "9edfafa7a5fc3dd95924",
+    "url": "/greekgraphs/static/js/main.908e30dd.chunk.js"
+  },
+  {
+    "revision": "fe423b9a166ed3c5bbbf",
+    "url": "/greekgraphs/static/js/runtime~main.6bf6a2d4.js"
+  },
+  {
+    "revision": "1cd807633400cf8fdf45474a390d4d31",
+    "url": "/greekgraphs/static/media/arrow.1cd80763.svg"
+  },
+  {
+    "revision": "1740fffd66a3765aa237288efcaa9d79",
+    "url": "/greekgraphs/static/media/logo.1740fffd.svg"
+  }
+]);
