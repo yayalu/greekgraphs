64a49fdf4717e39a7775d048135d84f687c35321
Yaya Lu <yaya.lu.01@gmail.com>
2019-11-28 14:01:38 +1100

    Fixed is older than error, and implemented placeholder solution for gender undefined

--- src/DataCardHandler.ts
+++ src/DataCardHandler.ts
@@ -55,15 +55,14 @@ let familyDatums = [
  ***************************/
 
 export const updateComponent = (id: string) => {
-  /* Preliminary information (i.e. name) about the entity */
-  let name = entities[id]["Name (Smith & Trzaskoma)"];
-  let type = entities[id]["Type of entity"];
-  let members: any[] = [];
+  let connections = getAllConnections(id);
+  return sortConnectionsIntoRelationships(id, connections);
+};
 
-  /*******************/
-  /* Find all relationships */
-  /*******************/
-
+/*******************/
+/* Find all relationships */
+/*******************/
+const getAllConnections = (id: string) => {
   var connections: {
     target: string;
     targetID: string;
@@ -152,9 +151,14 @@ export const updateComponent = (id: string) => {
       });
     }
   });
+  return connections;
+};
 
-  // Sort family relationships into their relevant relationship state categories
-  //TODO: deal with duplicates
+const sortConnectionsIntoRelationships = (id: string, connections: any) => {
+  /* Preliminary information (i.e. name) about the entity */
+  let name = entities[id]["Name (Smith & Trzaskoma)"];
+  let type = entities[id]["Type of entity"];
+  let members: any[] = [];
   let relationships: relationshipInfo = {
     MOTHERS: [],
     FATHERS: [],
@@ -164,6 +168,7 @@ export const updateComponent = (id: string) => {
     HUSBANDS: [],
     CHILDREN: []
   };
+  // Sort family relationships into their relevant relationship state categories
   connections.forEach(datum => {
     let d: entityInfo = {
       target: datum.target,
@@ -261,27 +266,29 @@ export const updateComponent = (id: string) => {
         relationships.HUSBANDS.push(d);
       }
     } else if (datum.verb === "marries") {
-      if (genderData[datum.targetID].gender === "female") {
-        let wasDuplicate = false;
-        relationships.WIVES.forEach(wives => {
-          if (wives.targetID === d.targetID) {
-            wives.passage.push(d.passage[0]);
-            wasDuplicate = true;
+      if (genderData) {
+        if (genderData[datum.targetID].gender === "female") {
+          let wasDuplicate = false;
+          relationships.WIVES.forEach(wives => {
+            if (wives.targetID === d.targetID) {
+              wives.passage.push(d.passage[0]);
+              wasDuplicate = true;
+            }
+          });
+          if (!wasDuplicate) {
+            relationships.WIVES.push(d);
           }
-        });
-        if (!wasDuplicate) {
-          relationships.WIVES.push(d);
-        }
-      } else if (genderData[datum.targetID].gender === "male") {
-        let wasDuplicate = false;
-        relationships.FATHERS.forEach(fathers => {
-          if (fathers.targetID === d.targetID) {
-            fathers.passage.push(d.passage[0]);
-            wasDuplicate = true;
+        } else if (genderData[datum.targetID].gender === "male") {
+          let wasDuplicate = false;
+          relationships.FATHERS.forEach(fathers => {
+            if (fathers.targetID === d.targetID) {
+              fathers.passage.push(d.passage[0]);
+              wasDuplicate = true;
+            }
+          });
+          if (!wasDuplicate) {
+            relationships.FATHERS.push(d);
           }
-        });
-        if (!wasDuplicate) {
-          relationships.FATHERS.push(d);
         }
       }
       // address collectives
@@ -326,8 +333,6 @@ export const updateComponent = (id: string) => {
     type: type,
     validSearch: true
   };
-
-  // Modify the relationship and name
 };
 
 /* Alphabetize the relationship */
@@ -357,58 +362,62 @@ export const checkNoRelations = (relationships: any) => {
 };
 
 const reversedVerb = (verb: string, dirObject: string) => {
-  if (
-    verb === "is mother of" ||
-    verb === "is father of" ||
-    verb === "is parent of"
-  ) {
-    return "is child of"; // Uses generic "is child of" at the moment since data cards do not need gender specificity for children
-  } else if (
-    verb === "is son of" ||
-    verb === "is daughter of" ||
-    verb === "is child of"
-  ) {
-    if (genderData[dirObject].gender === "female") {
-      return "is mother of";
-    } else if (genderData[dirObject].gender === "male") {
-      return "is father of";
+  if (genderData[dirObject]) {
+    if (
+      verb === "is mother of" ||
+      verb === "is father of" ||
+      verb === "is parent of"
+    ) {
+      return "is child of"; // Uses generic "is child of" at the moment since data cards do not need gender specificity for children
+    } else if (
+      verb === "is son of" ||
+      verb === "is daughter of" ||
+      verb === "is child of"
+    ) {
+      if (genderData[dirObject].gender === "female") {
+        return "is mother of";
+      } else if (genderData[dirObject].gender === "male") {
+        return "is father of";
+      } else {
+        return "is parent of";
+      }
+    } else if (verb === "is twin of") {
+      return "is twin of";
+    } else if (
+      verb === "is sister of" ||
+      verb === "is brother of" ||
+      verb === "is older than"
+    ) {
+      if (genderData[dirObject].gender === "female") {
+        return "is sister of";
+      } else if (genderData[dirObject].gender === "male") {
+        return "is brother of";
+      } else {
+        console.log("Gender-undefined sibling relationship");
+        return "";
+      }
+    } else if (
+      verb === "is wife of" ||
+      verb === "is husband of" ||
+      verb === "marries"
+    ) {
+      if (genderData[dirObject].gender === "female") {
+        return "is wife of";
+      } else if (genderData[dirObject].gender === "male") {
+        return "is husband of";
+      } else {
+        return "marries";
+      }
     } else {
-      return "is parent of";
-    }
-  } else if (verb === "is twin of") {
-    return "is twin of";
-  } else if (
-    verb === "is sister of" ||
-    verb === "is brother of" ||
-    verb === "is older than"
-  ) {
-    if (genderData[dirObject].gender === "female") {
-      return "is sister of";
-    } else if (genderData[dirObject].gender === "male") {
-      return "is brother of";
-    } else {
-      console.log("Gender-undefined sibling relationship");
+      console.log(
+        "Unsure of this connection, or connection is not relevant for the datacards.",
+        verb,
+        dirObject
+      );
       return "";
     }
-  } else if (
-    verb === "is wife of" ||
-    verb === "is husband of" ||
-    verb === "marries"
-  ) {
-    if (genderData[dirObject].gender === "female") {
-      return "is wife of";
-    } else if (genderData[dirObject].gender === "male") {
-      return "is husband of";
-    } else {
-      return "marries";
-    }
   } else {
-    console.log(
-      "Unsure of this connection, or connection is not relevant for the datacards.",
-      verb,
-      dirObject
-    );
-    return "";
+    return "undefined";
   }
 };
 
