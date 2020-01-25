62e03f2000bef635b17bec49388315568b5fcf37
Yaya Lu <yaya.lu.01@gmail.com>
2020-01-25 13:21:40 +1100

    Added scrollbar to graph

--- src/EntityGraph.jsx
+++ src/EntityGraph.jsx
@@ -6,7 +6,7 @@ import entities from "./data/entities.json";
 import passages from "./data/passages.json";
 import relationships from "./data/relationships.json";
 // import Konva from "konva";
-import {
+/* import {
   Stage,
   Layer,
   Label,
@@ -16,7 +16,7 @@ import {
   Group,
   Rect,
   Circle
-} from "react-konva";
+} from "react-konva"; */
 
 class EntityGraph extends React.Component {
   // Combining canvas tag with konva-react: https://lavrton.com/using-react-with-html5-canvas-871d07d8d753/
@@ -35,6 +35,14 @@ class EntityGraph extends React.Component {
     ctx.strokeRect(x, y, this.state.nodeWidth, this.state.nodeHeight);
     // ctx.font = "20px Arial";
     ctx.fillText(text, x + 10, y + 20);
+  }
+
+  edge(props) {
+    const { ctx, fromX, fromY, toX, toY } = props;
+    ctx.beginPath();
+    ctx.moveTo(fromX, fromY + this.state.nodeHeight / 2);
+    ctx.lineTo(toX, toY + this.state.nodeHorizontalSpacing / 2);
+    ctx.stroke();
   }
 
   componentDidUpdate() {
@@ -70,34 +78,66 @@ class EntityGraph extends React.Component {
         y2: mainNodeHeight + this.state.nodeHeight
       };
 
+      /***********************************/
+      /*  DEPTH 0 - SIBLINGS AND SPOUSES */
+      /***********************************/
+
       //Loop through all depth 0 nodes and add them into the graph
       let extension = 0;
       Object.values(graphContent.nodes).forEach(node => {
         if (node.depth === 0 && node.id !== this.props.id) {
-          console.log(node);
+          // The following means the main node is centred, and the other nodes fan out alongside the main node.
           if (extension % 2 === 0) {
+            let rightNodeX =
+              nodePositions[this.props.id].x2 +
+              nodeHorizontalSpacing +
+              nodeHorizontalSpacing * extension;
+            let rightNodeY = nodePositions[this.props.id].y1;
             this.node({
               ctx,
-              x:
-                nodePositions[this.props.id].x2 +
-                nodeHorizontalSpacing +
-                nodeHorizontalSpacing * extension,
-              y: nodePositions[this.props.id].y1,
+              x: rightNodeX,
+              y: rightNodeY,
               text: getName(entities[node.id])
             });
+            nodePositions[node.id] = {
+              x1: rightNodeX,
+              y1: rightNodeY,
+              x2: rightNodeX + nodeHorizontalSpacing,
+              y2: rightNodeY + nodeHorizontalSpacing
+            };
           } else {
+            let leftNodeX =
+              nodePositions[this.props.id].x1 -
+              nodeWidth -
+              nodeHorizontalSpacing -
+              nodeHorizontalSpacing * (extension - 1);
+            let leftNodeY = nodePositions[this.props.id].y1;
             this.node({
               ctx,
-              x:
-                nodePositions[this.props.id].x1 -
-                nodeWidth -
-                nodeHorizontalSpacing -
-                nodeHorizontalSpacing * (extension - 1),
-              y: nodePositions[this.props.id].y1,
+              x: leftNodeX,
+              y: leftNodeY,
               text: getName(entities[node.id])
             });
+            nodePositions[node.id] = {
+              x1: leftNodeX,
+              y1: leftNodeY,
+              x2: leftNodeX - nodeHorizontalSpacing,
+              y2: leftNodeY - nodeHorizontalSpacing
+            };
           }
           extension++;
+        }
+      });
+
+      Object.values(graphContent.edges).forEach(edge => {
+        if (edge.relation === "spouse") {
+          this.edge({
+            ctx,
+            fromX: nodePositions[edge.from].x1,
+            fromY: nodePositions[edge.from].y1,
+            toX: nodePositions[edge.to].x1,
+            toY: nodePositions[edge.to].y1
+          });
         }
       });
 
@@ -155,13 +195,22 @@ class EntityGraph extends React.Component {
             />
           </Layer>
     </Stage>*/}
-        <canvas
-          ref="graphCanvas"
-          id="responsive-canvas"
-          width={width}
-          height={height}
-          style={{ border: "1px solid #000000" }}
-        ></canvas>
+        <div
+          style={{
+            overflow: "scroll",
+            border: "1px solid #000000",
+            maxWidth: "100%",
+            maxHeight: "100%",
+            textAlign: "center"
+          }}
+        >
+          <canvas
+            ref="graphCanvas"
+            id="responsive-canvas"
+            width={5000}
+            height={500}
+          ></canvas>
+        </div>
       </div>
     );
   }
